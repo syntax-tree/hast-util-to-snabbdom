3791be4b85374c693610186c9ae6066dcbe945c0
Jannis R <mail@jannisr.de>
2017-12-04 18:22:12 +0100

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -1,8 +1,145 @@
 'use strict'
 
+const u = require('unist-builder')
+const s = require('snabbdom/h').default
 const test = require('tape')
 
-const todo = require('.')
+const toSnabbdom = require('.')
 
-test('todo', (t) => {
+test('should throw on invalid usage', (t) => {
+	t.throws(() => toSnabbdom())
+	t.throws(() => toSnabbdom('bar'))
+
+	t.end()
+})
+
+test('removes invalid & unknown falsy props', (t) => {
+	const uTree = u('element', {
+		tagName: 'p',
+		properties: {
+			ignored: false, // unknown booleans are ignored
+			disabled: 0 // falsy known booleans are ignored
+		}
+	}, [])
+
+	t.deepEqual(toSnabbdom(uTree), s('p', {}, []))
+	t.end()
+})
+
+test('handles id and classes', (t) => {
+	const uTree = u('element', {
+		tagName: 'p',
+		properties: {
+			id: 'a',
+			className: ['b', 'c']
+		}
+	}, [])
+
+	t.deepEqual(toSnabbdom(uTree), s('p#a.b.c', {}, []))
+	t.end()
+})
+
+test('handles styles', (t) => {
+	const uTree = u('element', {
+		tagName: 'p',
+		properties: {
+			style: 'color: red; background-color: blue'
+		}
+	}, [])
+	const expected = s('p', {
+		style: {
+			color: 'red',
+			'background-color': 'blue'
+		}
+	}, [])
+	// console.error(require('util').inspect(expected, {depth: Infinity}))
+
+	t.deepEqual(toSnabbdom(uTree), expected)
+	t.end()
+})
+
+test('handles case', (t) => {
+	const uTree = u('element', {
+		tagName: 'input',
+		properties: {
+			camelCase: 'foo', // unknown props are dash-cased
+			acceptCharset: 'bar', // known props are dash-cased
+			ariaValuenow: '1' // ARIA props
+		}
+	}, [])
+	const expected = s('input', {
+		'camel-case': 'foo',
+		'accept-charset': 'bar',
+		'aria-valuenow': '1'
+	}, [])
+
+	t.deepEqual(toSnabbdom(uTree), expected)
+	t.end()
+})
+
+test('handles data-*', (t) => {
+	const uTree = u('element', {
+		tagName: 'p',
+		properties: {
+			'data-foo': 'a',
+			'data-bar-baz': 'b'
+		}
+	}, [])
+	const expected = s('p', {
+		dataset: {
+			foo: 'a',
+			'bar-baz': 'b'
+		}
+	}, [])
+
+	t.deepEqual(toSnabbdom(uTree), expected)
+	t.end()
+})
+
+test('handles lists', (t) => {
+	const uTree = u('element', {
+		tagName: 'input',
+		properties: {
+			type: 'file',
+			foo: ['on', 'off'], // unknown lists are space-separated
+			accept: ['.jpg', '.jpeg'] // known lists are separated appropriately
+		}
+	}, [])
+	const expected = s('input', {
+		type: 'file',
+		foo: 'on off',
+		accept: '.jpg, .jpeg'
+	}, [])
+
+	t.deepEqual(toSnabbdom(uTree), expected)
+	t.end()
+})
+
+test('more complex tree', (t) => {
+	const uTree = u('element', {tagName: 'h1'}, [
+		u('text', 'hey there!'),
+		u('comment', 'i am a comment'),
+		u('element', {tagName: 'p'}, [
+			u('text', 'foo')
+		]),
+		u('text', 'bar'),
+		u('element', {
+			tagName: 'input',
+			properties: {
+				type: 'file',
+				name: 'baz'
+			}
+		}, [])
+	])
+
+	const expected = s('h1', {}, [
+		'hey there!',
+		s('p', {}, ['foo']),
+		'bar',
+		s('input', {type: 'file', name: 'baz'}, [])
+	])
+
+	const actualSTree = toSnabbdom(uTree)
+	t.deepEqual(actualSTree, expected)
+	t.end()
 })
