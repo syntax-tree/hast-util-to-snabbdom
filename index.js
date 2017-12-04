2970eaeca9fcd922bc053ac147c8a6be6c03db65
Jannis R <mail@jannisr.de>
2017-12-04 22:24:31 +0100

    change code to StandardJS

--- index.js
+++ index.js
@@ -8,87 +8,84 @@ const h = require('snabbdom/h').default
 const hasProp = Object.prototype.hasOwnProperty
 
 const divWrapper = (children) => {
-	return {type: 'element', tagName: 'div', properties: {}, children}
+  return {type: 'element', tagName: 'div', properties: {}, children}
 }
 
 const toSnabbdom = (uTree) => {
-	if (typeof uTree !== 'object' || Array.isArray(uTree)) {
-		throw new Error('uTree must be an object')
-	}
+  if (typeof uTree !== 'object' || Array.isArray(uTree)) {
+    throw new Error('uTree must be an object')
+  }
 
-	if (uTree.type === 'root') {
-		const c = uTree.children
-		if (!Array.isArray(c) || c.length === 0) return null
-		uTree = c.length === 1 ? c[0] : divWrapper(c)
-	} else if (uTree.type === 'comment') return null
+  if (uTree.type === 'root') {
+    const c = uTree.children
+    if (!Array.isArray(c) || c.length === 0) return null
+    uTree = c.length === 1 ? c[0] : divWrapper(c)
+  } else if (uTree.type === 'comment') return null
 
-	return convert(uTree)
+  return convert(uTree)
 }
 
 const convert = (node) => {
-	if (node.type === 'text') return node.value
-	// ignoring `comment` and `doctype` nodes
-	if (node.type !== 'element') return null
+  if (node.type === 'text') return node.value
+  // ignoring `comment` and `doctype` nodes
+  if (node.type !== 'element') return null
 
-	let selector = node.tagName
-	const data = {}
-	const dataset = Object.create(null)
+  let selector = node.tagName
+  const data = {}
+  const dataset = Object.create(null)
 
-	const props = node.properties || {}
-	if (props.id) selector += '#' + props.id
-	if (props.className) {
-		const c = props.className
-		selector += '.' + (Array.isArray(c) ? c.join('.') : c) // todo: empty arr
-	}
+  const props = node.properties || {}
+  if (props.id) selector += '#' + props.id
+  if (props.className) {
+    const c = props.className
+    selector += '.' + (Array.isArray(c) ? c.join('.') : c) // todo: empty arr
+  }
 
-	if (typeof props.style === 'string') {
-		if (props.style) data.style = parseStyle(props.style)
-	} else if (typeof props.style === 'object' && !Array.isArray(props.style)) {
-		data.style = props.style
-	}
+  if (typeof props.style === 'string') {
+    if (props.style) data.style = parseStyle(props.style)
+  } else if (typeof props.style === 'object' && !Array.isArray(props.style)) {
+    data.style = props.style
+  }
 
-	for (let prop in props) {
-		if (!hasProp.call(props, prop)) continue
-		if (prop === 'id' || prop === 'className' || prop === 'style') continue
+  for (let prop in props) {
+    if (!hasProp.call(props, prop)) continue
+    if (prop === 'id' || prop === 'className' || prop === 'style') continue
 
-		let val = props[prop]
-		if (prop.slice(0, 5) === 'data-') {
-			dataset[prop.slice(5)] = val
-			continue
-		}
+    let val = props[prop]
+    if (prop.slice(0, 5) === 'data-') {
+      dataset[prop.slice(5)] = val
+      continue
+    }
 
-		const info = propInfo(prop) || {}
+    const info = propInfo(prop) || {}
 
-		// ignore nully, `false`, `NaN` and falsey known booleans
-		if (
-			val === null ||
-			val === undefined ||
-			val === false ||
-			Number.isNaN(val) ||
-			(info.boolean && !val)
-		) continue
+    // ignore nully, `false`, `NaN` and falsey known booleans
+    if (
+      val === null ||
+      val === undefined ||
+      val === false ||
+      Number.isNaN(val) ||
+      (info.boolean && !val)
+    ) continue
 
-		if (Array.isArray(val)) {
-			val = info.commaSeparated ? val.join(', ') : val.join(' ')
-		}
+    if (Array.isArray(val)) {
+      val = info.commaSeparated ? val.join(', ') : val.join(' ')
+    }
 
-		data[info.name || kebabCase(prop)] = val
-	}
+    data[info.name || kebabCase(prop)] = val
+  }
 
-	const children = []
-	if (Array.isArray(node.children)) {
-		for (let child of node.children) {
-			const res = convert(child)
-			if (res !== null && res !== undefined) children.push(res)
-		}
-	}
+  const children = []
+  if (Array.isArray(node.children)) {
+    for (let child of node.children) {
+      const res = convert(child)
+      if (res !== null && res !== undefined) children.push(res)
+    }
+  }
 
-	for (let k in dataset) {
-		data.dataset = dataset
-		break
-	}
+  if (Object.keys(dataset).length > 0) data.dataset = dataset
 
-	return h(selector, data, children)
+  return h(selector, data, children)
 }
 
 module.exports = toSnabbdom
--- test.js
+++ test.js
@@ -7,170 +7,170 @@ const test = require('tape')
 const toSnabbdom = require('.')
 
 test('should throw on invalid usage', (t) => {
-	t.throws(() => toSnabbdom())
-	t.throws(() => toSnabbdom('bar'))
+  t.throws(() => toSnabbdom())
+  t.throws(() => toSnabbdom('bar'))
 
-	t.end()
+  t.end()
 })
 
 test('removes invalid & unknown falsy props', (t) => {
-	const uTree = u('element', {
-		tagName: 'p',
-		properties: {
-			ignored: false, // unknown booleans are ignored
-			disabled: 0 // falsy known booleans are ignored
-		}
-	}, [])
+  const uTree = u('element', {
+    tagName: 'p',
+    properties: {
+      ignored: false, // unknown booleans are ignored
+      disabled: 0 // falsy known booleans are ignored
+    }
+  }, [])
 
-	t.deepEqual(toSnabbdom(uTree), s('p', {}, []))
-	t.end()
+  t.deepEqual(toSnabbdom(uTree), s('p', {}, []))
+  t.end()
 })
 
 test('handles id and classes', (t) => {
-	const uTree = u('element', {
-		tagName: 'p',
-		properties: {
-			id: 'a',
-			className: ['b', 'c']
-		}
-	}, [])
+  const uTree = u('element', {
+    tagName: 'p',
+    properties: {
+      id: 'a',
+      className: ['b', 'c']
+    }
+  }, [])
 
-	t.deepEqual(toSnabbdom(uTree), s('p#a.b.c', {}, []))
-	t.end()
+  t.deepEqual(toSnabbdom(uTree), s('p#a.b.c', {}, []))
+  t.end()
 })
 
 test('handles styles', (t) => {
-	const uTree = u('element', {
-		tagName: 'p',
-		properties: {
-			style: 'color: red; background-color: blue'
-		}
-	}, [])
-	const expected = s('p', {
-		style: {
-			color: 'red',
-			'background-color': 'blue'
-		}
-	}, [])
-	// console.error(require('util').inspect(expected, {depth: Infinity}))
+  const uTree = u('element', {
+    tagName: 'p',
+    properties: {
+      style: 'color: red; background-color: blue'
+    }
+  }, [])
+  const expected = s('p', {
+    style: {
+      color: 'red',
+      'background-color': 'blue'
+    }
+  }, [])
+  // console.error(require('util').inspect(expected, {depth: Infinity}))
 
-	t.deepEqual(toSnabbdom(uTree), expected)
-	t.end()
+  t.deepEqual(toSnabbdom(uTree), expected)
+  t.end()
 })
 
 test('handles case', (t) => {
-	const uTree = u('element', {
-		tagName: 'input',
-		properties: {
-			camelCase: 'foo', // unknown props are dash-cased
-			acceptCharset: 'bar', // known props are dash-cased
-			ariaValuenow: '1' // ARIA props
-		}
-	}, [])
-	const expected = s('input', {
-		'camel-case': 'foo',
-		'accept-charset': 'bar',
-		'aria-valuenow': '1'
-	}, [])
+  const uTree = u('element', {
+    tagName: 'input',
+    properties: {
+      camelCase: 'foo', // unknown props are dash-cased
+      acceptCharset: 'bar', // known props are dash-cased
+      ariaValuenow: '1' // ARIA props
+    }
+  }, [])
+  const expected = s('input', {
+    'camel-case': 'foo',
+    'accept-charset': 'bar',
+    'aria-valuenow': '1'
+  }, [])
 
-	t.deepEqual(toSnabbdom(uTree), expected)
-	t.end()
+  t.deepEqual(toSnabbdom(uTree), expected)
+  t.end()
 })
 
 test('handles data-*', (t) => {
-	const uTree = u('element', {
-		tagName: 'p',
-		properties: {
-			'data-foo': 'a',
-			'data-bar-baz': 'b'
-		}
-	}, [])
-	const expected = s('p', {
-		dataset: {
-			foo: 'a',
-			'bar-baz': 'b'
-		}
-	}, [])
+  const uTree = u('element', {
+    tagName: 'p',
+    properties: {
+      'data-foo': 'a',
+      'data-bar-baz': 'b'
+    }
+  }, [])
+  const expected = s('p', {
+    dataset: {
+      foo: 'a',
+      'bar-baz': 'b'
+    }
+  }, [])
 
-	t.deepEqual(toSnabbdom(uTree), expected)
-	t.end()
+  t.deepEqual(toSnabbdom(uTree), expected)
+  t.end()
 })
 
 test('handles lists', (t) => {
-	const uTree = u('element', {
-		tagName: 'input',
-		properties: {
-			type: 'file',
-			foo: ['on', 'off'], // unknown lists are space-separated
-			accept: ['.jpg', '.jpeg'] // known lists are separated appropriately
-		}
-	}, [])
-	const expected = s('input', {
-		type: 'file',
-		foo: 'on off',
-		accept: '.jpg, .jpeg'
-	}, [])
+  const uTree = u('element', {
+    tagName: 'input',
+    properties: {
+      type: 'file',
+      foo: ['on', 'off'], // unknown lists are space-separated
+      accept: ['.jpg', '.jpeg'] // known lists are separated appropriately
+    }
+  }, [])
+  const expected = s('input', {
+    type: 'file',
+    foo: 'on off',
+    accept: '.jpg, .jpeg'
+  }, [])
 
-	t.deepEqual(toSnabbdom(uTree), expected)
-	t.end()
+  t.deepEqual(toSnabbdom(uTree), expected)
+  t.end()
 })
 
 test('handles `root` root node', (t) => {
-	const h1 = u('element', {
-		tagName: 'h1',
-		properties: {}
-	}, [
-		u('text', 'foo')
-	])
-	const p = u('element', {
-		tagName: 'p',
-		properties: {id: 'foo'}
-	}, [
-		u('text', 'bar')
-	])
+  const h1 = u('element', {
+    tagName: 'h1',
+    properties: {}
+  }, [
+    u('text', 'foo')
+  ])
+  const p = u('element', {
+    tagName: 'p',
+    properties: {id: 'foo'}
+  }, [
+    u('text', 'bar')
+  ])
 
-	const uTreeWith1 = u('root', {}, [p])
-	const uTreeWith2 = u('root', {}, [h1, p])
+  const uTreeWith1 = u('root', {}, [p])
+  const uTreeWith2 = u('root', {}, [h1, p])
 
-	t.deepEqual(toSnabbdom(u('root', {}, [])), null)
-	t.deepEqual(toSnabbdom(uTreeWith1), s('p#foo', {}, ['bar']))
-	t.deepEqual(toSnabbdom(uTreeWith2), s('div', {}, [
-		s('h1', {}, ['foo']),
-		s('p#foo', {}, ['bar'])
-	]))
-	t.end()
+  t.deepEqual(toSnabbdom(u('root', {}, [])), null)
+  t.deepEqual(toSnabbdom(uTreeWith1), s('p#foo', {}, ['bar']))
+  t.deepEqual(toSnabbdom(uTreeWith2), s('div', {}, [
+    s('h1', {}, ['foo']),
+    s('p#foo', {}, ['bar'])
+  ]))
+  t.end()
 })
 
 test('handles `comment` root node', (t) => {
-	t.equal(toSnabbdom(u('comment', 'foo')), null)
-	t.end()
+  t.equal(toSnabbdom(u('comment', 'foo')), null)
+  t.end()
 })
 
 test('more complex tree', (t) => {
-	const uTree = u('element', {tagName: 'h1'}, [
-		u('text', 'hey there!'),
-		u('comment', 'i am a comment'),
-		u('element', {tagName: 'p'}, [
-			u('text', 'foo')
-		]),
-		u('text', 'bar'),
-		u('element', {
-			tagName: 'input',
-			properties: {
-				type: 'file',
-				name: 'baz'
-			}
-		}, [])
-	])
+  const uTree = u('element', {tagName: 'h1'}, [
+    u('text', 'hey there!'),
+    u('comment', 'i am a comment'),
+    u('element', {tagName: 'p'}, [
+      u('text', 'foo')
+    ]),
+    u('text', 'bar'),
+    u('element', {
+      tagName: 'input',
+      properties: {
+        type: 'file',
+        name: 'baz'
+      }
+    }, [])
+  ])
 
-	const expected = s('h1', {}, [
-		'hey there!',
-		s('p', {}, ['foo']),
-		'bar',
-		s('input', {type: 'file', name: 'baz'}, [])
-	])
+  const expected = s('h1', {}, [
+    'hey there!',
+    s('p', {}, ['foo']),
+    'bar',
+    s('input', {type: 'file', name: 'baz'}, [])
+  ])
 
-	const actualSTree = toSnabbdom(uTree)
-	t.deepEqual(actualSTree, expected)
-	t.end()
+  const actualSTree = toSnabbdom(uTree)
+  t.deepEqual(actualSTree, expected)
+  t.end()
 })
